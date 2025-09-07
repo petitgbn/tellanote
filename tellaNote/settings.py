7681607282bade29597b566a0af664144160481e
petitgbn <gabnpetit5@gmail.com>
2025-09-07 18:20:31 +0000

    Update settings.py

--- tellaNote/settings.py
+++ tellaNote/settings.py
@@ -74,6 +74,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    ...
 ]
 
 ROOT_URLCONF = 'tellaNote.urls'
@@ -151,12 +153,15 @@ django.utils.encoding.force_text = force_str
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR , "./tella/static"),
     os.path.join(BASE_DIR , "./autentification/static/")
 
 ]
+
+# Dossier de sortie pour collectstatic (utilisé en production)
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # D
 # Default primary key field type
